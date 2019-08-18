e9cac2ee458c947efa320e1f7a907ddb3eab0930
Ethan Smith <ethan@onebytegone.com>
2019-08-18 17:11:03 -0400

    feat: basic canvas drawing

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,3 +3,20 @@ import Example from './Example';
 const example = new Example();
 
 console.log(example.doSomething());
+
+document.body.onload = function() {
+   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+   if (canvas) {
+      const ctx = canvas.getContext('2d');
+
+      if (ctx) {
+         ctx.fillStyle = 'rgb(200, 100, 0)';
+         ctx.fillRect(0, 0, 640, 360);
+         ctx.fillStyle = 'rgb(200, 0, 0)';
+         ctx.fillRect(10, 10, 50, 50);
+         ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+         ctx.fillRect(30, 30, 50, 50);
+      }
+   }
+};
